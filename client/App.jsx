d88578e00563390542a212b79958272d56dd239b
Carson Chen <carson.cy.chen@gmail.com>
2019-02-05 14:03:27 -0500

    webpack config completed

---
+++ client/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        <p>testing react</p>
+        <p>test</p>
+      </div>
+    )
+  }
+}
+
+export default App;
