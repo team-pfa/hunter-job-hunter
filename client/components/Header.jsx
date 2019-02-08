d80cfe49c88ff9a69af0e159055a7befa2ebac0b
Daniel <wdanni@Daniels-MacBook-Pro.local>
2019-02-08 18:00:04 -0500

    header component ok

---
+++ client/components/Header.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '/Users/wdanni/Desktop/codesmith/hunter-job-hunter/client/css/Header.css';
+
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>
+            <div class='flex-container'>
+                <div class='header-container' onClick={this.props.showPFAInfo}>
+                    <p id='about'>ABOUT</p>
+                </div>
+                <div class='header-container' onClick={this.props.showActivity}>
+                    <p id='activity'>ACTIVITY</p>
+                </div >
+                <div class='header-container' onClick={this.props.showUserInfo}>
+                    <p id='username'>{this.props.username}</p>
+                </div>
+                <div>
+                    <img src={this.props.avatar} id='avatar'></img>
+                </div>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default Header;
+
+
+
